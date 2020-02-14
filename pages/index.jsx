4c1e8be0cb2a6a329a1f567ef00aef1da90d9a49
wangchen <2090861117@qq.com>
2020-02-14 21:50:54 +0800

    add antd

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
-import '../test.css'
+import {Button} from 'antd'
 
 export default function index() {
   return (
     <div>
       Hello World
+      <Button>aaa</Button>
     </div>
   )
 }
