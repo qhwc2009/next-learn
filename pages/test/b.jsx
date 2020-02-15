a09587d9a33af2c6dc6bb4505eb2f1b707c2c5f3
wangchen <2090861117@qq.com>
2020-02-15 22:33:53 +0800

    next.config.js里面的配置项相关

--- pages/test/b.jsx
+++ pages/test/b.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic';
+import getConfig from 'next/config';
 
 const LazyComp = dynamic(import('../../components/LazyComp'));
 
+const {serverRuntimeConfig, publicRuntimeConfig} = getConfig();
+
 function PageB({ name, time }) {
   const router = useRouter();
+  // eslint-disable-next-line
+  console.log('serverRuntimeConfig: ', serverRuntimeConfig)
+  // eslint-disable-next-line
+  console.log('publicRuntimeConfig: ', publicRuntimeConfig)
   return (
     <div style={{ backgroundColor: '#eaeaea' }}>
       <Head>
@@ -16,6 +23,7 @@ function PageB({ name, time }) {
       <div>name: {name}</div>
       <div>time: {time}</div>
       <LazyComp />
+      <div>Custom key: {process.env.customKey}</div>
     </div>
   );
 }
