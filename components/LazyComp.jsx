bbc5491dbb741fdb96fbc0432551df4a2a07638f
wangchen <2090861117@qq.com>
2020-02-15 22:13:06 +0800

    异步加载模块和组件, 代码分割

---
+++ components/LazyComp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LazyComp() {
+  return (
+    <div>
+      Lazy component
+    </div>
+  )
+}
+
+export default LazyComp
--- pages/test/b.jsx
+++ pages/test/b.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
-
 import { useRouter } from 'next/router';
+import dynamic from 'next/dynamic';
 
-function PageB() {
+const LazyComp = dynamic(import('../../components/LazyComp'));
+
+function PageB({ name, time }) {
   const router = useRouter();
   return (
     <div style={{ backgroundColor: '#eaeaea' }}>
@@ -11,12 +13,25 @@ function PageB() {
         <title>My page b</title>
       </Head>
       {router.query.id}
+      <div>name: {name}</div>
+      <div>time: {time}</div>
+      <LazyComp />
     </div>
   );
 }
 
 PageB.getInitialProps = async () => {
-  return {};
+  // 异步加载模块
+  const moment = await import('moment');
+  const promise = new Promise(resolve => {
+    setTimeout(() => {
+      resolve({
+        name: 'chen ge',
+        time: moment.default(Date.now() - 60 * 1000).fromNow(),
+      });
+    }, 1000);
+  });
+  return await promise;
 };
 
 export default PageB;
