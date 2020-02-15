73a5b02880b9585f254cf74fef25fe7a618d2253
wangchen <2090861117@qq.com>
2020-02-15 15:35:13 +0800

    自定义app组件

---
+++ components/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Link from 'next/link';
+import { Button } from 'antd';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <header>
+        <Link href="/">
+          <Button>To Home</Button>
+        </Link>
+        <Link href="/test/b?id=1" as="test/b/1">
+          <Button>To B1</Button>
+        </Link>
+      </header>
+      {children}
+    </>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
 import App from 'next/app';
-
+import Layout from '../components/Layout';
 import 'antd/dist/antd.css';
 
-export default App;
+class MyApp extends App {
+  static async getInitialProps({ Component }) {
+    // 必须要拿一下Component的getInitialProps 不然的话 组件的getInitialProps就不会生效
+    let pageProps;
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps();
+    }
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    );
+  }
+}
+
+export default MyApp;
